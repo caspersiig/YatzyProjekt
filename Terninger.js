1d2fc81e78271b790c2f96fd124b17a11a7d3452
TAULO <taulo@live.dk>
2021-09-15 12:43:34 +0200

    din røv er sindssyg

--- Terninger.js
+++ Terninger.js
@@ -1,3 +1,30 @@
 const turn = document.getElementById("turn");
 const roll = document.getElementById("roll");
-console.log("hej med dig:DDD")
+
+let terning = document.getElementsByClassName("terning");
+
+let randNum = () => {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+let randImg = () => {
+
+}
+
+window.onload = () => {
+    terning[0].firstChild.src = "billeder/dice-six-faces-five.svg";
+    console.log(terning[1].src);
+}
+
+let turnValue = 2;
+let rolled = () => {
+    if (turnValue <= 3) {
+        randImg();
+        turn.innerHTML = "Turn " + turnValue++;
+    } else {
+        console.log("ingen ture tilbage");
+    }
+}
+
+roll.addEventListener("click", rolled);
+
